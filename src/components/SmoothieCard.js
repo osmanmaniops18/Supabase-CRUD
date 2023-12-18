afa0da0ecb351485d391f9da09d3aa51352b8e2e
Muhammad Usman Haider <musmanvk18@gmail.com>
2023-12-18 20:57:35 +0000

    complete CRUD with supabase

---
+++ src/components/SmoothieCard.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
+const SmoothieCard = ({ smoothie }) => {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from('to_do_task')
+      .delete()
+      .eq('id', smoothie.id)
+    
+    if (error) {
+      console.log(error)
+    }
+    if (data) {
+      console.log(data)
+    }
+  }
+    return (
+      <div className="smoothie-card">
+        <h3>{smoothie.title}</h3>
+        <p>{smoothie.method}</p>
+        <div className="rating">{smoothie.rating}</div>
+        <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+        <i className="material-icons" onClick={handleDelete}>delete</i>
+      </div>
+      </div>
+    )
+  }
+  
+  export default SmoothieCard
---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import supabase from "../config/supabaseClient"
+import { useEffect } from "react"
+import SmoothieCard from "../components/SmoothieCard"
+
+const Home = () => {
+  const [error,setError]=useState(null)
+  const [data,setData] = useState(null)
+
+
+useEffect(() => {
+
+const fetchData=async()=>{
+  const {data,error}=await supabase
+  .from('to_do_task')
+  .select()
+  if (error) {
+    setError("could not fetch data")
+    setData(null)
+    console.log(error)
+  }
+  if (data) {
+    setData(data)
+    setError(null)
+  }
+}
+
+fetchData()
+
+}, [])
+
+  return (
+    <div className="page home">
+      {error && (<p>{error}</p>)}
+      {data && (
+        <div className="smoothies">
+          {/* order-by buttons */}
+          <div className="smoothie-grid">
+            {data.map(item => (
+              <SmoothieCard key={item.id} smoothie={item} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Home
